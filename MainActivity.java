c0e34558f7709fca62e3f4cd013b580d7576480e
Thomas Phillips <thomasphillips3@gmail.com>
2015-07-14 20:50:29 -0700

    - Moved start and stop recording logic to dedicated methods (startRecording() and stopRecording())

--- MainActivity.java
+++ MainActivity.java
@@ -20,6 +20,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 import java.io.IOException;
+import java.io.File;
 
 import android.util.Log;
 
@@ -27,7 +28,11 @@ public class MainActivity extends Activity {
 
     Button btnRecord, btnSend, btnPlayback;
     private MediaRecorder myAudioRecorder;
-    private String recordedPhrase = null;
+    int num = 0;
+    private String recordedPhrase = "recording_" + num + ".3gp";
+    private File recording = new File(Environment.getExternalStorageDirectory(), recordedPhrase);
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,56 +48,77 @@ public class MainActivity extends Activity {
         btnPlayback.setEnabled(false);
         btnSend.setEnabled(false);
 
-        // Set the file name and path to output
-        recordedPhrase = Environment.getExternalStorageDirectory().getAbsolutePath() + "/idgaf.mp4";
-        ;
-
-        // Set various audio parameters
-        myAudioRecorder = new MediaRecorder();
-        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.MPEG_4);
-        myAudioRecorder.setOutputFile(recordedPhrase);
-
+        // Run this method when Record button is pressed down
         btnRecord.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent recordBtnPressed) {
+
                 // Check to see if button is pressed
                 switch(recordBtnPressed.getAction() & MotionEvent.ACTION_MASK){
                     // When button is held down
                     case MotionEvent.ACTION_DOWN:
+//                        recordedPhrase = null;
                         btnRecord.setPressed(true);
-                        // Record audio while button is held
-                        try {
-                            myAudioRecorder.prepare();
-                            myAudioRecorder.start();
-                            Toast.makeText(getApplication(), "Audio recording", Toast.LENGTH_LONG).show();
-                            Log.v("TOUCH EVENT", recordBtnPressed.toString());
-                        } catch (IllegalStateException e) {
-                            e.printStackTrace();
-                        } catch (IOException e){
-                            e.printStackTrace();
-                        }
+                        Log.v("TOUCH EVENT", "button pressed");
+                        startRecording();
                         break;
 
                     // When button is released
                     case MotionEvent.ACTION_UP:
+                        stopRecording();
                         btnRecord.setPressed(false);
-                        try {
-                            myAudioRecorder.stop();
-                            myAudioRecorder.release();
-                            Toast.makeText(getApplication(), "Recording finished", Toast.LENGTH_LONG).show();
-                            Log.v("TOUCH EVENT", recordBtnPressed.toString());
-                            Log.v("FILE EVENT", "File saved to: " + recordedPhrase.toString());
-                        } catch (IllegalStateException e2){
-                            e2.printStackTrace();
-                        }
+                        Log.v("TOUCH EVENT", "button released");
                         break;
                 }
-
                 return true;
             }
         });
+    }
+
+    private void startRecording() {
+
+//        File recording = new File(Environment.getExternalStorageDirectory(), recordedPhrase);
+
+        // Set various audio parameters
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+
+        Log.i("METHOD", "Recording exists. File is called: " + recordedPhrase);
+        recordedPhrase = "audioRecording_" + num + ".3gp";
+//            recording = new File(Environment.getExternalStorageDirectory(), recordedPhrase);
+        myAudioRecorder.setOutputFile(recordedPhrase);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+
+        // Record audio while button is held
+        try {
+            // Start recording
+            myAudioRecorder.prepare();
+            myAudioRecorder.start();
+            Log.i("METHOD", "Audio recorder prepared");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        Toast.makeText(getApplication(), "Audio recording", Toast.LENGTH_LONG).show();
+    }
+
+    private void stopRecording() {
+        num++;
+        try {
+            Log.i("METHOD", "Entered 'stopRecording' block");
+            myAudioRecorder.stop();
+            myAudioRecorder.release();
+            myAudioRecorder = null;
+
+        } catch (IllegalStateException e){
+            e.printStackTrace();
+        }
+
+        Toast.makeText(getApplication(), "Recording finished", Toast.LENGTH_LONG).show();
+        Log.v("TOUCH EVENT", btnRecord.toString());
+        Log.v("FILE EVENT", "File saved to: " + Environment.getExternalStorageDirectory() + "/" + recordedPhrase);
+
     }
 }
 
